5473a6eeb44385f00983ea6ba4e8832bf53aea48
Davin <65677163+davin2020@users.noreply.github.com>
2024-04-16 21:47:50 +0100

    added main(), tidied comments, moved regex so its compiled only once, changed some var names

--- process_vtt_files.py
+++ process_vtt_files.py
@@ -1,52 +1,42 @@
-#  PY script to process vtt files, remove any blank lines and timestamps,
-# then add carragie returns after full stops - so transcript is easier to read
+# Python script to process .vtt transcript files from videos, remove any blank lines and timestamps,
+# then add carraige returns after full stops of each sentence, so transcript is easier to read.
 
 import re
 import os
 
-# finds timestamps in vtt file in the format - 00:00:19.850
-TIMESTAMP_REGEX = "^\d\d:\d\d:\d\d\.\d\d\d"
-# remove number from vtt file, in format from 1 to 9999
-NUMBER_REGEX = "^[0-9]{1,4}$"
+# Best to compile regex only once, ouside of any loops, then can use it multiple times inside loops
+# Finds timestamps in vtt file in the format - 00:00:19.850 - ie under 1 hr in duration
+# TODO what would 1 hr+ video timestamp look like?
+TIMESTAMP_REGEX = re.compile("^\d\d:\d\d:\d\d\.\d\d\d")
+# carat ^ mean beginning of line
 
+# remove sequential number from vtt file, in format from 1 to 9999
+NUMBER_REGEX = re.compile("^[0-9]{1,4}$")
 
-# NEXT try this against Trauma Conf files tha ti cant watch, but can get VTT for 
-FOLDER = "test_process"
+# adjust name of folder as needed - TODO could pass in folder name as param to script
+FOLDER = "files_to_process"
 
 def proces_vtt(my_filename):
-	print("my_filename :: ", my_filename)
 	INPUT_FILENAME = FOLDER + "/" + my_filename
+	(just_filename, extention) = os.path.splitext(my_filename)
 
-	# TODO change processed filename to .txt 
-	(just_filename, ext) = os.path.splitext(my_filename)
-	print("filename without ext : ", just_filename)
-
+	# changed processed filename to .txt - 
+	# caveat - any existing files with same name will be overwritten
 	OUTPUT_FILENAME = FOLDER + "/" +  "Processed_" + just_filename + ".txt"
-	print("\n")
-	print("filename INPUT_FILENAME ",  INPUT_FILENAME)
-	print("filename OUTPUT_FILENAME ",  OUTPUT_FILENAME)
 
 	input_file = open(INPUT_FILENAME, 'r')
 	input_lines = input_file.readlines()
 	 
-	#  try diff WITH syntax?
-	# with open(INPUT_FILENAME) as file:
-	#   print(file.readlines())
-	#   # output_file.writelines(input_lines)
-
-	# print("...............")
 	all_text = []
 
-	#  check if copes with URLs  eg `xyz.com` in original transcript  - maybe serach for `x. ` ie extra space at end?
-	# Strips the newline character
+	# process each line of input file, saving wanted text to a list all_text[]
 	for line in input_lines:
-
+		# strip removes newline char (but print adds them back)
 		if line.strip(): 
-			# regex for timestamp
-			line_with_number = re.findall(NUMBER_REGEX, line)
+			# regex for matching against numbers, or timestamp 
+			line_with_number = NUMBER_REGEX.match(line)
 			# print("NUMBER_REGEX line_with_number: ", line_with_number)
-
-			line_with_timestamp = re.findall(TIMESTAMP_REGEX, line)
+			line_with_timestamp = TIMESTAMP_REGEX.match(line)      
 			# print("line_with_timestamp: ", line_with_timestamp)
 
 			if not line_with_timestamp:
@@ -55,37 +45,31 @@ def proces_vtt(my_filename):
 					if "WEBVTT" not in line:
 						all_text.append(line)
 
-
-	# print("\ndone adding lines to all_text[] ")
-	# print(all_text)   
-
-	cleaned_text = []
+	# formatting text by adding newline after each sentence, so its not all one large paragraph
+	formatted_text = []
 	for item in all_text:
 		line_with_space = item.replace("\n", " ")
-		# need to match on full stop then space, otherwise elipses like .. get replaced
+		# need to match on full stop then space, otherwise elipses like .. get replaced new with newline
 		sentence_with_newline = line_with_space.replace(". ", ". \n")
-		cleaned_text.append(sentence_with_newline)
+		formatted_text.append(sentence_with_newline)
 
-	print("done updating cleaned_text[]] ")
-
-	# TODO NEXT - 
-	# add 1 space to end of every line, and remove existing \n - or replace \n with space?
-	# add carraige  ereturn after full stop 
-
-	# # writing to file
+	# write formtated text to file
 	output_file = open(OUTPUT_FILENAME, 'w')
-	output_file.writelines(cleaned_text)
+	output_file.writelines(formatted_text)
 
 	# output_file.writelines(input_lines)
-	print("processed vtt file and saved output to txt file")
-	print("output filename: ", OUTPUT_FILENAME)
+	print("\nProcessed .vtt file and saved output to .txt file")
+	print("Output filename: ", OUTPUT_FILENAME)
 	input_file.close()
 	output_file.close()
 
 
-#  add main here
+def main():
+	for filename in os.listdir(FOLDER):
+		if filename.endswith(".vtt"):
+			proces_vtt(filename)
 
-# for filename in os.scandir(FOLDER):
-for filename in os.listdir(FOLDER):
-	if filename.endswith(".vtt"):
-		proces_vtt(filename)
+
+if __name__ == '__main__':
+	main()
+    
